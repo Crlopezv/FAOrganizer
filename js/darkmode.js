55ebc85d2d2640ef8621b88bc23737779b8b443e
Cristobal_Lopez <cr.lopezv@duocuc.cl>
2022-05-26 02:28:41 -0400

    Modo oscuro agregado

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,29 @@
 
 function DarkMode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+   var SetTheme = document.body;
+   SetTheme.classList.toggle("dark-mode")
+   var theme;
+   if(SetTheme.classList.contains("dark-mode")){
+       console.log("Dark mode");
+       theme = "DARK";
+   }else{
+       console.log("Light mode");
+       theme = "LIGHT";
+   }
+   
+   // Salvar a almacenamiento local:
+
+   localStorage.setItem("PageTheme", JSON.stringify(theme));
+   
 }
+
+setInterval(() => {
+   let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+   console.log(GetTheme);
+   if(GetTheme === "DARK"){
+       document.body.classList = "dark-mode";
+   }else{
+       document.body.classList = "";
+   }
+}, 5);
+
