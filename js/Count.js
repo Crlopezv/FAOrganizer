cbfbd45b6e859cbccfebc7068f89a49435a7f077
Cristobal_Lopez <cr.lopezv@duocuc.cl>
2022-05-26 05:11:59 -0400

    Agregando más funcionas al js

--- js/Count.js
+++ js/Count.js
@@ -1,6 +1,22 @@
+//Chequear caracteres restantes
+
 function textareaLengthCheck(el) {
     var textArea = el.value.length;
     var charactersLeft = 250 - textArea;
     var count = document.getElementById('lblRemainingCount');
     count.innerHTML = "Carácteres restantes: " + charactersLeft;
   }
+
+//Enviar formulario
+
+function enviar() {
+  document.getElementById("formulariocontact").submit();
+  alert ("Formulario enviado")
+}
+
+//Resetear formulario (WIP)
+
+function reset() {
+  document.getElementById("formulariocontact").reset();
+
+}
